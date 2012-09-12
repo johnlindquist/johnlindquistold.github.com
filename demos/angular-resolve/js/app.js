94f87c1f5705cdaf3a5b00a3da5aaf41cbf5f7fb
John Lindquist <johnlindquist@gmail.com>
2012-09-12 01:46:09 -0600

    Testing a demos dir

---
+++ demos/angular-resolve/js/app.js
@@ -0,0 +1,99 @@
+var app = angular.module('app', []);
+
+app.config(function ($locationProvider, $routeProvider) {
+    $locationProvider.html5Mode(true);
+
+    $routeProvider.when("/", {templateUrl:"partials/home.html"});
+    $routeProvider.when("/cat", {
+        templateUrl:"partials/cat.html",
+        controller :"CatCtrl",
+        resolve    :{
+            toy:function ($timeout, $q) {
+                var defer = $q.defer();
+
+                //delay the response for 2 seconds, then succeed
+                $timeout(function () {
+                    defer.resolve(
+                        {type:"yarn"}
+                    );
+                }, 2000);
+
+                return defer.promise;
+            }
+        }
+
+    });
+    $routeProvider.when("/dog", {
+        templateUrl:"partials/dog.html",
+        controller:"DogCtrl",
+        resolve    :{
+            toy:function ($timeout, $q) {
+                var defer = $q.defer();
+
+                //delay the response for 2 seconds, then reject
+                $timeout(function () {
+                    defer.reject("Dogs don't deserve toys!");
+                }, 2000);
+
+                return defer.promise;
+            }
+        }
+    });
+
+    //Even silly demo apps need Easter Eggs :)
+    $routeProvider.when("/aquaman", {templateUrl:"partials/aquaman.html"});
+});
+
+function CatCtrl($scope, toy) {
+    //notice that this is triggered AFTER the route has successfull changed
+    //this means you can prepare any data in the "toy" that you want
+    alert("The cat likes: " + toy.type);
+}
+function DogCtrl($scope, toy) {
+    //this will never happen since we're intentionally failing the route change
+    alert("The dog likes: " + toy.type);
+}
+
+function AppCtrl($scope, $rootScope, $location) {
+    $rootScope.$on("$routeChangeStart", function (event, next, current) {
+        $scope.alertType = "alert-info";
+        $scope.alertMessage = "Changing routes";
+    });
+    $rootScope.$on("$routeChangeSuccess", function (event, current, previous) {
+        $scope.alertType = "alert-success";
+        $scope.alertMessage = "Successfully changed routes :)";
+    });
+    $rootScope.$on("$routeChangeError", function (event, current, previous, rejection) {
+        alert(rejection + " - Sending you back home");
+        $scope.alertType = "alert-error";
+        $scope.alertMessage = "Failed to change routes :(";
+
+        $location.path("/");
+    });
+
+    $scope.alertType = "alert-info";
+    $scope.alertMessage = "Welcome to the resolve demo";
+
+    $scope.tabs = [
+        {
+            title:"Home",
+            url  :"/"
+        },
+        {
+            title:"Cats Succeed",
+            url  :"/cat"
+        },
+        {
+            title:"Dogs Fail",
+            url  :"/dog"
+        }
+    ];
+
+    $scope.checkActive = function (url) {
+        if (url == $location.path()) {
+            return "active";
+        } else {
+            return "";
+        }
+    };
+}
