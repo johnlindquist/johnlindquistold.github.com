69eda2598e276b6038e573f79f668665dc527f48
johnlindquist <johnlindquist@gmail.com>
2012-10-16 01:43:34 -0600

    testing the flash cards project

---
+++ cards/js/controllers.js
@@ -0,0 +1,51 @@
+'use strict';
+
+/* Controllers */
+
+
+function CardsCtrl($scope, $http) {
+  $scope.cards = $http.get("https://spreadsheets.google.com/feeds/list/0ApDPbpSNCArIdDctTnl4RWExUGlwdnY4ZlJUZUl1YUE/1/public/basic?alt=json")
+    .success(function (data) {
+
+      setTimeout(setupReveal, 1000);
+    });
+
+}
+
+function setupReveal(){
+  Reveal.initialize({
+    // Display controls in the bottom right corner
+    controls: true,
+
+    // Display a presentation progress bar
+    progress: true,
+
+    // Push each slide change to the browser history
+    history: false,
+
+    // Enable keyboard shortcuts for navigation
+    keyboard: true,
+
+    // Enable the slide overview mode
+    overview: true,
+
+    // Loop the presentation
+    loop: false,
+
+    // Number of milliseconds between automatically proceeding to the
+    // next slide, disabled when set to 0, this value can be overwritten
+    // by using a data-autoslide attribute on your slides
+    autoSlide: 0,
+
+    // Enable slide navigation via mouse wheel
+    mouseWheel: true,
+
+    // Apply a 3D roll to links on hover
+    rollingLinks: true,
+
+    // Transition style
+    transition: 'default' // default/cube/page/concave/zoom/linear/none
+  });
+
+}
+
