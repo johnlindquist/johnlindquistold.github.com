7d1328c47d24438e090c26425045c3a1bdcbf220
johnlindquist <johnlindquist@gmail.com>
2012-10-29 15:42:22 -0600

    shuffling the cards

--- cards/js/controllers.js
+++ cards/js/controllers.js
@@ -4,48 +4,50 @@
 
 
 function CardsCtrl($scope, $http) {
-  $scope.cards = $http.get("https://spreadsheets.google.com/feeds/list/0ApDPbpSNCArIdDctTnl4RWExUGlwdnY4ZlJUZUl1YUE/1/public/basic?alt=json")
-    .success(function (data) {
+    $http.get("https://spreadsheets.google.com/feeds/list/0ApDPbpSNCArIdDctTnl4RWExUGlwdnY4ZlJUZUl1YUE/1/public/basic?alt=json")
+        .success(function (data) {
+            var cards = _.shuffle(data.feed.entry);
 
-      setTimeout(setupReveal, 1000);
-    });
+            $scope.cards = cards;
+            setTimeout(setupReveal, 1000);
+        });
 
 }
 
-function setupReveal(){
-  Reveal.initialize({
-    // Display controls in the bottom right corner
-    controls: true,
+function setupReveal() {
+    Reveal.initialize({
+        // Display controls in the bottom right corner
+        controls:true,
 
-    // Display a presentation progress bar
-    progress: true,
+        // Display a presentation progress bar
+        progress:true,
 
-    // Push each slide change to the browser history
-    history: false,
+        // Push each slide change to the browser history
+        history:false,
 
-    // Enable keyboard shortcuts for navigation
-    keyboard: true,
+        // Enable keyboard shortcuts for navigation
+        keyboard:true,
 
-    // Enable the slide overview mode
-    overview: true,
+        // Enable the slide overview mode
+        overview:true,
 
-    // Loop the presentation
-    loop: false,
+        // Loop the presentation
+        loop:false,
 
-    // Number of milliseconds between automatically proceeding to the
-    // next slide, disabled when set to 0, this value can be overwritten
-    // by using a data-autoslide attribute on your slides
-    autoSlide: 0,
+        // Number of milliseconds between automatically proceeding to the
+        // next slide, disabled when set to 0, this value can be overwritten
+        // by using a data-autoslide attribute on your slides
+        autoSlide:0,
 
-    // Enable slide navigation via mouse wheel
-    mouseWheel: true,
+        // Enable slide navigation via mouse wheel
+        mouseWheel:true,
 
-    // Apply a 3D roll to links on hover
-    rollingLinks: true,
+        // Apply a 3D roll to links on hover
+        rollingLinks:true,
 
-    // Transition style
-    transition: 'default' // default/cube/page/concave/zoom/linear/none
-  });
+        // Transition style
+        transition:'none' // default/cube/page/concave/zoom/linear/none
+    });
 
 }
 
